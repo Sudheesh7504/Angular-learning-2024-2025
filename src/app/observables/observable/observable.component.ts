6765742603e7313d73f2c45b7ef344982b3f92f8
Sudheesh Kurupati <ksudeesh143@gmail.com>
2025-02-17 11:22:02 +0530

    observables

--- src/app/observables/observable/observable.component.ts
+++ src/app/observables/observable/observable.component.ts
@@ -13,6 +13,10 @@ export class ObservableComponent {
   errorMsg!: string;
   isCompleted: boolean = false;
   obs1Subs: any;
+  obs2: any;
+  nums: any;
+  obs2Subs: any;
+
 
   constructor(private cdr: ChangeDetectorRef) { }
   click() {
@@ -56,9 +60,44 @@ export class ObservableComponent {
     });
   }
 
+  ngOnInit() {
+    this.nums = [];
+    this.obs2 = new Observable((obs2) => { //if we want to do any task after completetion of this observable then this will helpful
+      obs2.next(1);
+      obs2.next(10);
+      obs2.next(56);
+      obs2.next(12);
+      obs2.next(16);
+      // obs2.error("error occured");
+      obs2.complete();
+    });
+
+    this.obs2Subs = this.obs2.subscribe(({
+      next: (data: any) => {
+        console.log(this.nums);
+        this.nums?.push(data);
+      },
+      complete: () => {
+        let sum = 0;
+        for (let i = 0; i < this.nums?.length; i++) {
+          sum += this.nums[i];
+        }
+        console.log("nums array sum", sum);
+      },
+      error: (err: any) => {
+        console.log("error", err);
+      }
+    }))
+
+
+  }
+
   ngOnDestroy() {
     if (this.obs1Subs) {
       this.obs1Subs.unSubscribe();
+    }
+    if (this.obs2Subs) {
+      this.obs2Subs.unSubscribe();
     }
   }
 
